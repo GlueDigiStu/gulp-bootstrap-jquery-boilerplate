d30fecb01e3565dac8ee1696e7a2790469a6828c
Alex Jeffers <alex@gluestudio.co.uk>
2017-04-27 22:52:45 +0100

    initial commit

---
+++ app/js/scripts.js
@@ -0,0 +1,16 @@
+/*!
+ * fastshell
+ * Fiercely quick and opinionated front-ends
+ * https://HosseinKarami.github.io/fastshell
+ * @author Hossein Karami
+ * @version 1.0.5
+ * Copyright 2017. MIT licensed.
+ */
+//Assign jQuery to the $ symbol
+jQuery(document).ready(function ($) {
+  'use strict';  //Use strict for all functions within the document ready function
+  $(function () {
+
+
+  });
+});
---
+++ src/js/scripts.js
@@ -0,0 +1,8 @@
+//Assign jQuery to the $ symbol
+jQuery(document).ready(function ($) {
+  'use strict';  //Use strict for all functions within the document ready function
+  $(function () {
+
+
+  });
+});
